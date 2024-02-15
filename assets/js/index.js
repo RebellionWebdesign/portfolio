fe202b01b5f1c2d8560fec72202cb6eab568d9bf
Daniel Stauffer <info@rebellion-webdesign.com>
2024-02-15 12:19:08 +0100

    Add cursor effects

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,4 +8,32 @@ document.addEventListener("DOMContentLoaded", (event) => {
             link.classList.add('active');
         }
     });
+
+    document.addEventListener("mousemove", (e) => {
+        const cursor = document.querySelector(".cursor")
+        const button = document.querySelector(".button")
+        const link = document.getElementsByTagName("a")
+        let x = e.pageX
+        let y = e.pageY
+
+        const buttonRect = button.getBoundingClientRect()
+        const targetX = buttonRect.left + buttonRect.width / 2;
+        const targetY = buttonRect.top + buttonRect.height / 2;
+        const deltaX = x - targetX
+        const deltaY = y - targetY
+        const distance = Math.sqrt(deltaX*deltaX+deltaY*deltaY)
+        const threshold = 100
+
+        cursor.style.top = y + "px"
+        cursor.style.left = x + "px"
+        cursor.style.display = "block";
+
+        if (distance < threshold) {
+            cursor.style.transform = 'scale(2)'
+            cursor.style.backgroundColor = 'orange';
+        } else {
+            cursor.style.transform = 'scale(1)'
+            cursor.style.backgroundColor = '#f8d500';
+        }
+    })
 });
